ca5235c58b0781a168d0bf75d4de029162779a62
djo1975 <mladanilic1975@gmail.com>
2023-02-13 15:46:25 +0100

    test-api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
